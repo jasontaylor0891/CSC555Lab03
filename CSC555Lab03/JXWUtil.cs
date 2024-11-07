8118d84261aba979fe9c532aa5424517181faee9
Jason Taylor <jason.taylor0891@gmail.com>
2024-11-07 17:48:50 -0500

    Add project files.

---
+++ CSC555Lab03/JXWUtil.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC555Lab03
+{
+    public class JXWUtil
+    {
+
+        static char[] allowableKeyChars = new char[]
+        {
+            '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b',
+            'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'm',
+            'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+            'y', 'z'
+        };
+
+        public static bool CheckLicence(string email, string key)
+        {
+            key = key.Trim().ToLower();
+            if (TestKey(email, key))
+                return true;
+
+            int atIndex = email.IndexOf('@');
+            if (atIndex > -1)
+            {
+                string companyName = email.Substring(atIndex + 1);
+                if (TestKey(companyName, key))
+                    return true;
+
+                while (companyName.IndexOf('.') > 0)
+                {
+                    companyName = companyName.Substring(companyName.IndexOf('.') + 1);
+                    if (TestKey(companyName, key))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        private static bool TestKey(string licenceName, string key)
+        {
+            return key.Equals(GenerateCode(licenceName, true)) ||
+                   key.Equals(GenerateCode(licenceName, false));
+        }
+
+        public static string GenerateCode(string seed)
+        {
+            return GenerateCode(seed, true);
+        }
+
+        public static string GenerateCode(string seed, bool forceLowerCase)
+        {
+            string bloop = seed.Trim();
+            if (forceLowerCase)
+                bloop = bloop.ToLower();
+
+            int hash = CalculateHash(bloop);
+            return HashToLicenceString(hash);
+        }
+
+        private static string HashToLicenceString(int seed)
+        {
+            var vNumber = new System.Text.StringBuilder("jx");
+            for (int i = 0; i < 6; i++)
+            {
+                char c = allowableKeyChars[seed & 0x1F];
+                vNumber.Insert(2, c);
+                seed >>= 5;
+            }
+            return vNumber.ToString();
+        }
+
+        public static int CalculateHash(string input)
+        {
+            int h = 0;
+            int len = input.Length;
+            for (int i = 0; i < len; i++)
+            {
+                h = 31 * h + input[i];
+            }
+            return h;
+        }
+    }
+
+}
---
+++ CSC555Lab03/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Mail;
+
+namespace CSC555Lab03
+{
+    internal class Program
+    {
+
+        private static bool IsEmailValid(string email)
+        {
+            bool valid = true;
+
+            try
+            {
+                var emailAddress = new MailAddress(email);
+            }
+            catch
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Email Address to create key for: ");
+            string emailAddress = Console.ReadLine();
+            if (!IsEmailValid(emailAddress))
+            {
+                Console.WriteLine("You must enter a valid email address");
+                System.Environment.Exit(1);
+            }
+            string key = JXWUtil.GenerateCode(emailAddress);
+            bool testKey = JXWUtil.CheckLicence(emailAddress, key);
+            Console.WriteLine($"License Key : {key}");
+            Console.WriteLine($"Key is valid: {testKey}");
+        }
+    }
+}
